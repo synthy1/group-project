f833fee2bba5730cdc8663a32ad3c56e5715195a
synthy1 <jackhopson69@gmail.com>
2023-01-10 20:21:19 +0000

    small changes

--- Assets/Scripts/NPC_Turn.cs
+++ Assets/Scripts/NPC_Turn.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NPC_Turn : MonoBehaviour
-
+{
+    Camera cam;
     // Update is called once per frame
+
+    void Start()
+    {
+        cam = GameObject.Find("Player camera").GetComponent<Camera>();
+    }
     void Update()
     {
-        transform.rotation = Quaternion.Euler(0f,Camera.main.transform.rotation.eulerAngles.y,0f);
+        transform.rotation = Quaternion.Euler(0f,cam.transform.rotation.eulerAngles.y,0f);
     }
 }
