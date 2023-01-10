d34858cc029827afa2b45593487fadbdb857db38
synthy1 <jackhopson69@gmail.com>
2023-01-10 12:45:15 +0000

    adding diologue

---
+++ Assets/Scripts/Diologue/Diologue_Trigger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diologue_Trigger : MonoBehaviour
+{
+    public Message[] messages;
+    public Actor[] actors;
+}
+
+
+    //creates class for actor message and image
+    [System.Serializable]
+    public class Message
+    {
+        public int actorId;
+        public string message;
+    }
+    
+    [System.Serializable]
+    public class Actor
+    {
+        public string name;
+        public Sprite sprite;
+    }
+
