88315659f2e5e14a6210576c38e90128f5f38e9b
synthy1 <113103296+synthy1@users.noreply.github.com>
2023-01-19 12:46:13 +0000

    TEST 1 READY

--- Assets/Scripts/Interaction System/Lvl1_Collectable.cs
+++ Assets/Scripts/Interaction System/Lvl1_Collectable.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Lvl1_Collectable : Interactable
 {
     Movement player;
     public GameObject PH_1;
     public GameObject PH_2;
+    public GameObject rats;
+    public GameObject walls;
+    MeshRenderer[] meshes;
+    public Material eyes;
     private void Start()
     {
         player = GameObject.Find("Player").GetComponent<Movement>();
@@ -18,6 +23,13 @@ public class Lvl1_Collectable : Interactable
 
     public override void OnInteract()
     {
+        meshes = walls.GetComponentsInChildren<MeshRenderer>();
+
+        foreach(MeshRenderer mesh in meshes)
+        {
+            mesh.material = eyes;
+        }
+        rats.SetActive(false);
         Destroy(gameObject);
         player.lvl1Cmp = true;
         PH_1.SetActive(false);
