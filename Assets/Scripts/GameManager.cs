1e9c6b4c6d3aeb10a6374440e6b46c62811e8b03
synthy1 <jackhopson69@gmail.com>
2023-01-25 23:59:44 +0000

    polish

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,45 +6,55 @@ public class GameManager : MonoBehaviour
 {
     public GameObject lvl1;
     public GameObject lvl2;
+    public GameObject lvl3;
+    public GameObject lvl4;
     public GameObject lobbySounds;
     Movement playerCmp;
     // Start is called before the first frame update
     void Start()
     {
         playerCmp= GameObject.Find("Player").GetComponent<Movement>();
+        lvl2.SetActive(false);
+        lvl3.SetActive(false);
+        lvl4.SetActive(false);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         //managing cluster
-        if (playerCmp.lvl1Cmp == true)
+        if (playerCmp.lvl1Cmp == true && playerCmp.inLobby == false)
         {
             Destroy(lvl1);
-        }
-
-        if (playerCmp.lvl2Cmp == true)
-        {
-            Destroy(lvl2);
-        } 
-
-        
-        if(playerCmp.lvl1Cmp == false || playerCmp.inLobby == true)
-        {
-            lvl2.SetActive(false);
-        }
-        else
-        {
             lvl2.SetActive(true);
         }
+
+        if (playerCmp.lvl2Cmp == true && playerCmp.inLobby == false)
+        {
+
+            Destroy(lvl2);
+            lvl3.SetActive(true);
+        }
+        if (playerCmp.lvl3Cmp == true && playerCmp.inLobby == false)
+        {
+
+            Destroy(lvl3);
+            lvl4.SetActive(true);
+
+        }
+
 
         if(playerCmp.inLobby== true)
         {
             lobbySounds.SetActive(true);
+            lvl2.SetActive(false);
+            lvl3.SetActive(false);
+            lvl4.SetActive(false);
         }
         else
         {
             lobbySounds.SetActive(false);
+
         }
     }
 }
